4ce9e5bb9fccb6eb5ec3ce308bdf9a23d61397b1
Aleksandre Gordeladze <aleqsandre1515@gmail.com>
2021-12-13 01:37:09 +0400

    initial code

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import random
+
+random.seed(20)
+
+# number of cluster was found by elbow method, with finding the
+# optimal point where graph decrease drastically slows down
+number_of_clusters = 9
+
+df = pd.read_excel('Book1.xlsx', sheet_name='data')
+
+km = KMeans(n_clusters=number_of_clusters)
+
+y_predicted = km.fit_predict(df[['longitude', 'latitude']])
+df['cluster'] = y_predicted
+
+dfs = []
+for i in range(number_of_clusters):
+    tmp = df[df.cluster == i]
+    dfs.append(tmp)
+
+random_color = [[round(random.uniform(0.1, 0.9), 2), round(random.uniform(0.1, 0.9), 2),
+                 round(random.uniform(0.1, 0.9), 2)] for i in
+                range(number_of_clusters)]
+
+for k in range(number_of_clusters):
+    plt.scatter(dfs[k]['longitude'], dfs[k]['latitude'],
+                color=(random_color[k][0], random_color[k][1], random_color[k][2]))
+
+plt.scatter(km.cluster_centers_[:, 0], km.cluster_centers_[:, 1], color='black', marker='*', label='centroid')
+
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+plt.legend()
+plt.show()
+# The results are reproducible, all points remain fixed at their locations.
+# It's just for the sake of beauty each region's color changes every time you compile code.
